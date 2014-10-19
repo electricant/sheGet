940fbe360ea25e9c92c46ecc610d42c7ec8fc34b
electricant <electricant@anche.no>
2014-10-19 16:15:19 +0200

    Add the scripts and their config file.

---
+++ sheget-makedb.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# Create the database for sheGet. The database is in the following form:
+# URL (string, primary key, unique) | priority (integer) | complete (integer)
+#
+if [ -z "$1" ]; then
+	echo Please provide a valid path for the database
+	echo USAGE:
+	echo sheget-makedb.sh path/to/sqlite.db
+	exit 1
+fi
+
+sqlite3 $1 'CREATE TABLE downloads(
+	url TEXT PRIMARY KEY UNIQUE NOT NULL,
+	priority INTEGER NOT NULL,
+	complete INTEGER NOT NULL
+);'
